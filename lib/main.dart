764f3819d1eef941d8a9bcf5fd7aaa5f00f095e8
Subritt <subrittburlakoti@gmail.com>
2020-07-13 21:47:34 +0545

    added fonts folder, defined a font family in pubspec.yaml and extended StatelessWidget class for hot reload

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,35 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('GroceryList'),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Text('Hello world!'),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Text('click'),
+      home: Home(),
+    ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('GroceryList'),
+        centerTitle: true,
+        backgroundColor: Colors.greenAccent,
+      ),
+      body: Center(
+        child: Text(
+          'Hello world!',
+          style: TextStyle(
+            fontSize: 26.0,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'IndieFlower',
+            letterSpacing: 2.0,
+            color: Colors.grey[800],
+          ),
         ),
       ),
-    ));
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('click'),
+        backgroundColor: Colors.greenAccent,
+      ),
+    );
+  }
+}
